a5f1b44e8b5137e7b728e88fc685cc6a225e4d20
Victor-Barbosa <victorbarbosadev@gmail.com>
2022-06-15 16:04:23 -0300

    Inicio Projeto

---
+++ deveficiente/src/main/java/br/com/casadocodigo/deveficiente/DeveficienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.casadocodigo.deveficiente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveficienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveficienteApplication.class, args);
+	}
+
+}
